0420042ecc5c9836cda4d697d00042b2b36da18a
kigawa <kigawa.8390@gmail.com>
2022-02-28 21:37:11 +0900

    fix: dependency

--- src/main/java/net/kigawa/net/ddnsclient/DDNSClient.java
+++ src/main/java/net/kigawa/net/ddnsclient/DDNSClient.java
@@ -1,7 +1,9 @@
 package net.kigawa.net.ddnsclient;
 
-import net.kigawa.util.Formatter;
-import net.kigawa.util.Logger;
+import net.kigawa.kutil.kutil.app.Application;
+import net.kigawa.kutil.kutil.file.FileUtil;
+import net.kigawa.kutil.log.log.Logger;
+import net.kigawa.kutil.terminal.Terminal;
 import net.kigawa.yamlutil.Yaml;
 
 import java.io.BufferedReader;
@@ -14,13 +16,15 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.logging.Level;
 
-public class DDNSClient {
+public class DDNSClient extends Application {
     public static final File current = Paths.get("").toAbsolutePath().toFile();
     public static final File ownIp = new File(current, "ownIp");
     public static final File config = new File(current, "config.yml");
-    public static final Logger logger;
+    public static final File logDir = FileUtil.getRelativeFile("logs");
     public static DDNSClient ddnsClient;
     public static boolean isEnd = true;
+    public static Logger logger;
+
     private final IpFile ipFile;
     private final IpTimer ipTimer;
     private final Yaml yaml;
@@ -28,28 +32,16 @@ public class DDNSClient {
     private URL checkIp;
     private Cloudflare cloudflare;
 
-    static {
-        java.util.logging.Logger.getLogger("").getHandlers()[0].setFormatter(new Formatter());
-        Logger.enable(DDNSClient.class.getName(), null, Level.INFO, Paths.get("").toAbsolutePath().toFile());
-        logger = Logger.getInstance();
-    }
-
     public DDNSClient() {
-        logger.info("start DDNSClient");
+        logger = new Logger(DDNSClient.class.getName(), null, Level.INFO, logDir);
+        addModule(logger);
+        addModule(new Terminal(true, logger));
+
         ipFile = new IpFile(this);
         ipTimer = new IpTimer(ipFile, this);
-        yaml = new Yaml(Util.getAbsolutFile());
-        logger.info("load data...");
-        data = yaml.load(ConfigData.class, config);
-        if (data == null) {
-            data = new ConfigData();
-            yaml.save(data);
-        }
-        logger.info("new cloudflare...");
-        cloudflare = new Cloudflare(data.getZoneId(), data.getId(), data.getEMail(), data.getKey());
+        yaml = new Yaml(Util.getAbsolutFile(), logger);
 
-        logger.info("start timer");
-        ipTimer.start();
+        enable();
     }
 
     public static void main(String[] args) {
@@ -94,22 +86,12 @@ public class DDNSClient {
         ipTimer.end();
     }
 
-    public void newURL(int count) {
-        if (count > 5) return;
-        try {
-            checkIp = new URL("http://checkip.amazonaws.com");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            newURL(count + 1);
-        }
+    public Cloudflare getCloudflare() {
+        return cloudflare;
     }
 
     public ConfigData getData() {
         return data;
-    }
-
-    public Cloudflare getCloudflare() {
-        return cloudflare;
     }
 
     public String getIp() {
@@ -125,7 +107,38 @@ public class DDNSClient {
         }
     }
 
-    public void updateIp(){
+    public void newURL(int count) {
+        if (count > 5) return;
+        try {
+            checkIp = new URL("http://checkip.amazonaws.com");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            newURL(count + 1);
+        }
+    }
+
+    @Override
+    protected void onDisable() {
+
+    }
+
+    @Override
+    protected void onEnable() {
+        logger.info("start DDNSClient");
+        logger.info("load data...");
+        data = yaml.load(ConfigData.class, config);
+        if (data == null) {
+            data = new ConfigData();
+            yaml.save(data);
+        }
+        logger.info("new cloudflare...");
+        cloudflare = new Cloudflare(data.getZoneId(), data.getId(), data.getEMail(), data.getKey());
+
+        logger.info("start timer");
+        ipTimer.start();
+    }
+
+    public void updateIp() {
         logger.info("update ip...");
 
     }
